d0e805a6fe69161f28fa484b44a08863e1c0c39c
favouritesam <favsamlol@gmail.com>
2026-06-23 17:10:12 +0100

    project setup

---
+++ src/main/java/com/stylesmart/StyleSmartsApplication.java
@@ -0,0 +1,12 @@
+package com.stylesmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleSmartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StyleSmartsApplication.class, args);
+    }
+}
